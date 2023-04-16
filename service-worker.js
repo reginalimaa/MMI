b78d7e1dc0047ce48d6a346f3a84a812b767ce32
reginalimaa <mariardlima@fac.pe.senac.br>
2023-04-15 21:28:45 -0300

    alteracoes pwa

--- service-worker.js
+++ service-worker.js
@@ -7,17 +7,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-        './dahsboard.html',
-        './digitaCodigo.html',
+        
         './generalInformation.html',
         './help.html',
-        './history.html',
-        './home.html',
+        './Home.html',
         './index.html',
         './machineIdentification.html',
         './requestMaintenance.html',
-        './scanner.html',
-        './typeMaintenance.html',
 
         './assets/css/reset.css',
         './assets/css/style.css',
@@ -26,21 +22,19 @@ self.addEventListener('install', event => {
         './assets/css/index.css',
         './assets/css/help.css',
 
-        './assets/loader.js',
-
         './assets/img/logoLogin.svg',
         './assets/img/logo.svg',
-        './assets/img/dashboard.svg',
-        './assets/img/digitaCod.svg',
+        './assets/img/dashboard.png',
+        './assets/img/dictation.png',
         './assets/img/menuHamburger.png',
-        './assets/img/ajuda.svg',
-        './assets/img/manutencao.svg',
-        './assets/img/manutencaoCorretiva.svg',
-        './assets/img/manutencaoPreventiva.sgv',
-        './assets/img/manuntencaoSensitiva.svg',
-        './assets/img/maquina.svg',
-        './assets/img/qrCode.svg',
-        './assets/img/upload.svg',
+        './assets/img/help.png',
+        './assets/img/manutencao.png',
+        './assets/img/manutencaoCorretiva.png',
+        './assets/img/manutencaoPreventiva.png',
+        './assets/img/manutencaoSensitiva.png',
+        './assets/img/maquina.png',
+        './assets/img/qrCode.png',
+        './assets/img/upload.png',
       ]))
   );
 });
